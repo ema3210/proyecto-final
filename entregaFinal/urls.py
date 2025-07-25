d7b65dfc3b637917d99c6eebc03a99eb4c22ab68
Emanuel Morales <ema.mrs@hotmail.com>
2025-07-25 01:12:53 -0300

    agregamos el readme del proyecto final

--- entregaFinal/urls.py
+++ entregaFinal/urls.py
@@ -19,5 +19,5 @@ from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('mi-primer-app/', include('mi_primer_app.urls'))
+    path('', include('mi_primer_app.urls'))
 ]
